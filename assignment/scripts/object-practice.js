e3de34e353df0675201065fd75d9aa2516d35f49
Laokeng Vang <laokengv@gmail.com>
2023-04-18 20:22:07 -0500

    Second commit. Completed codes for number 2 and 3.

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -24,21 +24,23 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+let fullName = 'Laokeng Vang'
+if (me.firstName && me.lastName === fullName){
 
+}
+console.log('My full name is', fullName);
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
-
-
+console.log(me.faveThreeFoods[0]);
+console.log(me.faveThreeFoods[1]);
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
